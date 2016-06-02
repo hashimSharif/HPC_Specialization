fdbda065ddb794cd1050044493b8279b5a95f955
hashim <hsharif3@illinois.edu>
2016-06-01 19:05:28 -0700

    Fixing script - no sudo access required to execute PIN

--- bin/pinOpcodePass.sh
+++ bin/pinOpcodePass.sh
@@ -1,2 +1,2 @@
-sudo PIN/pin -t PIN/source/tools/SimpleExamples/obj-intel64/opcodemix.so -o PIN_out/$2 -- $1
+../PIN/pin -t PIN/source/tools/SimpleExamples/obj-intel64/opcodemix.so -o PIN_out/$2 -- $1
 
