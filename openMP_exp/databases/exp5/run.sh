6097c6e6aea274e2bd3954348e51220edfc4e558
hsharif3 <hsharif3@illinois.edu>
2016-08-20 03:02:01 -0500

    Adding experiment 5 run script

--- openMP_exp/databases/exp5/run.sh
+++ openMP_exp/databases/exp5/run.sh
@@ -2,29 +2,27 @@
 
 module load hpctoolkit
 
-rm -r databases/exp5/database*
-rm -r prof/exp5/test*
-
-tests=( 1 7 )
 if [[ $1 == "build" ]] ;
 then
-  for test_num in "${tests[@]}"
-  do
-    hpclink CC src/test${test_num}.c -o build/test${test_num} -openmp
-    hpcstruct build/test${test_num} -o hpcstruct/test${test_num}.hpcstruct
-  done  
+  hpclink CC src/test1.c -o build/test1 -openmp
+  hpclink CC src/test2.c -o build/test2 -openmp
+  hpclink CC src/test3.c -o build/test3 -openmp
+  hpcstruct build/test1 -o hpcstruct/test1.hpcstruct
+  hpcstruct build/test2 -o hpcstruct/test2.hpcstruct
+  hpcstruct build/test3 -o hpcstruct/test3.hpcstruct
 fi
 
 thread_nums=( 2 4 8 12 24 )
 
 for thread_num in "${thread_nums[@]}"
 do
-  for test_num in "${tests[@]}"
-  do 
-    echo $thread_num , $test_num
-    export OMP_NUM_THREADS=$thread_num
-    srun -n 1 hpcrun -o prof/exp5/test${test_num}_${thread_num} --trace -e WALLCLOCK@5000  ./build/test${test_num}
-    hpcprof -S hpcstruct/test${test_num}.hpcstruct  prof/exp5/test${test_num}_${thread_num} -o databases/exp5/database_test${test_num}_${thread_num}
-  done
+  echo $thread_num
+  export OMP_NUM_THREADS=$thread_num
+  srun -n 4 -N 4 hpcrun -o prof/test1_$thread_num --trace -e WALLCLOCK@5  ./build/test1
+  srun -n 1 hpcrun -o prof/test2_$thread_num --trace -e WALLCLOCK@5  ./build/test2
+  srun -n 1 hpcrun -o prof/test3_$thread_num --trace -e WALLCLOCK@5  ./build/test3
+  hpcprof -S hpcstruct/test1.hpcstruct  prof/test1_$thread_num -o databases/database_test1_$thread_num
+  hpcprof -S hpcstruct/test2.hpcstruct  prof/test2_$thread_num -o databases/database_test2_$thread_num
+  hpcprof -S hpcstruct/test3.hpcstruct  prof/test3_$thread_num -o databases/database_test3_$thread_num
 done
 
