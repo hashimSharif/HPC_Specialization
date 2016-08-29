de0c95dbb0ff070b237163363c9deea9f54a0e43
hashim <hsharif3@illinois.edu>
2016-08-28 22:02:02 -0700

    Run script for edison testing larger number of threads <=48

--- openMP_exp/run.sh
+++ openMP_exp/run.sh
@@ -2,11 +2,11 @@
 
 module load hpctoolkit
 
-EXP=exp12
+EXP=exp17
 rm -r databases/$EXP/database*
 rm -r prof/$EXP/test*
 
-tests=( 1 2 3 8 9 )
+tests=( 2 9 )
 if [[ $1 == "build" ]] ;
 then
   for test_num in "${tests[@]}"
@@ -16,16 +16,20 @@ then
   done  
 fi
 
-thread_nums=( 2 4 6 8 11 18 23 )
+thread_nums=( 1 4 8 16 24 32 47 )
+total_threads=( 2 5 9 17 25 33 48 )
+i=0
 
 for thread_num in "${thread_nums[@]}"
 do
   for test_num in "${tests[@]}"
   do 
     echo $thread_num , $test_num
+    echo "threads allocated ", ${total_threads[$i]}
     export OMP_NUM_THREADS=$thread_num
-    srun -n 1 hpcrun -o prof/$EXP/test${test_num}_${thread_num} --trace -e  REALTIME@5000  ./build/test${test_num}
+    srun -n 1 -c ${total_threads[$i]}  hpcrun -o prof/$EXP/test${test_num}_${thread_num} --trace -e  REALTIME@1000  ./build/test${test_num}
     hpcprof -S hpcstruct/test${test_num}.hpcstruct  prof/$EXP/test${test_num}_${thread_num} -o databases/$EXP/database_test${test_num}_${thread_num}
   done
+  i=$i+1
 done
 
