1685d9e92ad4298016436f991da5b27e3fb157a2
hashimSharif <hashim.sharif91@gmail.com>
2016-09-26 18:56:16 -0700

    Adding test script for openMP transformations. Includes switches for rebuilding openMP runtime, building bitcode, executing openMP+MPI programs

--- Intel_openMP_opt/bin/run_openMP_transforms.sh
+++ Intel_openMP_opt/bin/run_openMP_transforms.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Load the required modules. 
+# PrgEnv-intel is required to use libomp.so instead of libgomp.so (GNU implementation)
 module unload PrgEnv-gnu
 module load PrgEnv-intel
 module load impi
@@ -14,6 +16,8 @@ export OMP_NUM_THREADS=8 # setting default for number of openMP threads per para
 
 # Add paths for clang-3.8 llvm-3.8
 export PATH=~/software/llvm-3.8.0.src/build/Release+Asserts/bin/:$PATH
+
+# Set mpicc spefic environment to use clang (instead of gcc)
 export OMPI_CC=clang
 export OMPI_MPICC=clang
 export OMPI_CXX=clang++
@@ -21,9 +25,46 @@ export OMPI_CXX=clang++
 # Add path for modified mpicc wrapper - support for clang enabled
 export PATH=$work_dir/mpicc_bin/:$PATH
 
+if [[ $1 == "build_runtime" ]] ;
+then
+  module load cmake
+  cd build
+  cmake ../
+  make clean
+  make
+  cd ../
+fi
+
+tests=( MPI_waitall )
+
+if [[ $1 == "build" ]] ;
+then
+  for test in "${tests[@]}"
+  do
+    mpicc -cc=clang -c -emit-llvm custom_tests/src/${test}.c -o custom_tests/bc/${test}.bc -fopenmp
+    llvm-dis custom_tests/bc/${test}.bc -o custom_tests/ll/${test}.ll
+  done
+fi
+
+if [[ $1 == "run" ]] ;
+then
+  for test in "${tests[@]}"
+  do
+    llvm-as custom_tests/ll/${test}.ll -o custom_tests/bc/${test}.bc
+    mpicc -cc=clang custom_tests/bc/${test}.bc -o custom_tests/bin/${test} -fopenmp
+    mpirun -n 2 custom_tests/bin/${test} &> custom_tests/logs/${test}.log
+  done
+fi
+
+
+#mpicc -cc=clang -c -emit-llvm custom_tests/MPI_waitall.c -o custom_tests/MPI_waitall_clang.bc -fopenmp
+#mpicc -cc=clang custom_tests/MPI_waitall_clang.bc -o custom_tests/MPI_waitall_clang -fopenmp
+#mpirun -n 2 custom_tests/MPI_waitall_clang
+
 # Build and execute with the mpic wrappers
-mpicc -cc=clang custom_tests/MPI_waitall.c -o custom_tests/MPI_waitall_clang -fopenmp
-mpirun -n 2 custom_tests/MPI_waitall_clang
+
+#mpicc -cc=clang custom_tests/MPI_waitall.c -o custom_tests/MPI_waitall_clang -fopenmp
+#mpirun -n 2 custom_tests/MPI_waitall_clang
 
 
 #CC -S custom_tests/MPI_waitall.c -o custom_tests/MPI_waitall.s -openmp
